3d97e07ac53a47c9282d10a7d48d04038496d30b
Trushangpatel-13 <ptrushang@gmail.com>
2021-04-11 09:44:13 +0530

    Update settings.py

--- Real_Time_Tracker Django App/Tracker/Tracker/settings.py
+++ Real_Time_Tracker Django App/Tracker/Tracker/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '!^tni!sgx9%n#z8dbm01cwxsl6&^0&zs9-_avl+t4!mnsgwgne'
+SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
