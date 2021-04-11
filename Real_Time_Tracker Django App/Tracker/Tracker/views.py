f3964abf36afa28e3bda68c8c73b4b738ecc4110
Trushangpatel-13 <ptrushang@gmail.com>
2021-04-11 09:44:43 +0530

    Update views.py

--- Real_Time_Tracker Django App/Tracker/Tracker/views.py
+++ Real_Time_Tracker Django App/Tracker/Tracker/views.py
@@ -5,16 +5,7 @@ from datetime import datetime
 from .scrapper import ads
 import time
 
-config = {
-  "apiKey": "AIzaSyDxQyfFPYsD27uHpOPGp9FC2VxlxVSEmsA",
-  "authDomain": "news-471f1.firebaseapp.com",
-  "databaseURL": "https://news-471f1.firebaseio.com",
-  "projectId": "news-471f1",
-  "storageBucket": "news-471f1.appspot.com",
-  "messagingSenderId": "579352149196",
-  "appId": "1:579352149196:web:75c2b568d65623cdbed669",
-  "measurementId": "G-YT4FLNBL4N"
-};
+
 
 firebase = pyrebase.initialize_app(config)
 
